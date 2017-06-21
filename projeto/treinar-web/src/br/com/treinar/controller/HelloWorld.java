4fbd678c3b5e1a8ffb6eb9904784bd3916e15052
TREINAR <TREINAR@MICRO52>
2017-06-20 22:46:35 -0300

    projeto web segunda aula

---
+++ projeto/treinar-web/src/br/com/treinar/controller/HelloWorld.java
@@ -0,0 +1,35 @@
+package br.com.treinar.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns = "*.trn")
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+	public void init() throws ServletException {
+		// Do required initialization
+		message = "Hello World [$(nome)]";
+
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		out.println("<h1>" + message.replace("$(nome)", nome) + "</h1>");
+
+	}
+
+	public void destroy() {
+		// do nothing.
+	}
+}
